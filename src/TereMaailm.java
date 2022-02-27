4a16e14fb41487f9cd756d9db1c0a4d892d3c1d9
karl Ruul <karlkevin.ruul@gmail.com>
2022-02-27 15:42:40 +0200

    sõber ikka siin

--- src/TereMaailm.java
+++ src/TereMaailm.java
@@ -1,6 +1,6 @@
 package PACKAGE_NAME;
 public class TereMaailm {
     public static void main(String[] args) {
-        System.out.println("Sõber oli siin");
+        System.out.println("Sõber ikka siin");
     }
 }
